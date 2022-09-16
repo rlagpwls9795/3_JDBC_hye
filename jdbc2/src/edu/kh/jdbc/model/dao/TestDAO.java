39cc68f673ae57d620a607f7a57887e17fe189f7
rlagpwls9795 <rlagpwls9795@naver.com>
2022-09-16 12:50:43 +0900

    20220916 JDBC

---
+++ jdbc2/src/edu/kh/jdbc/model/dao/TestDAO.java
@@ -0,0 +1,14 @@
+package edu.kh.jdbc.model.dao;
+
+import java.sql.Connection;
+
+import edu.kh.jdbc.model.vo.TestVO;
+
+public class TestDAO {
+
+	public int insert(Connection conn, TestVO vo1) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ jdbc2/src/edu/kh/jdbc/run/Run.java
@@ -0,0 +1,19 @@
+package edu.kh.jdbc.run;
+
+import edu.kh.jdbc.model.service.TestService;
+import edu.kh.jdbc.model.vo.TestVO;
+
+public class Run {
+	public static void main(String[] args) {
+		
+		TestService service= new TestService();
+		
+		// TB_TEST 테이블에 INSERT 수행
+		TestVO vo1 = new TestVO(1,"제목1","내용1");
+		
+		// TB_TEST 테이블에 INSERT를 수행하는 서비스 메서드를 호출 후 결과 반환 받기	
+		
+		int result=service.insert(vo1);
+		
+	}
+}
